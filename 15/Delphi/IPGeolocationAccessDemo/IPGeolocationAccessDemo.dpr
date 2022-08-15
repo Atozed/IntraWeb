a5e10193c7e97b3a0521833e7a113ad318913fc5
Alexandre Machado <alexandre.machado@softtech.com>
2022-08-16 11:04:55 +1200

    Missing file

---
+++ 15/Delphi/IPGeolocationAccessDemo/IPGeolocationAccessDemo.dpr
@@ -0,0 +1,14 @@
+program IPGeolocationAccessDemo;
+
+uses
+  IWRtlFix,
+  IWStart,
+  Unit30 in 'Unit30.pas' {IWForm30: TIWAppForm},
+  ServerController in 'ServerController.pas' {IWServerController: TIWServerControllerBase},
+  UserSessionUnit in 'UserSessionUnit.pas' {IWUserSession: TIWUserSessionBase};
+
+{$R *.res}
+
+begin
+  TIWStart.Execute(True);
+end.
