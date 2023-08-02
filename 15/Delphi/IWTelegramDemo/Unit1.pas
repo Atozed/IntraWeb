3029bdd5ff1b4a88af6821c188c44518a9374023
Alexandre Machado <alexandre.machado@softtech.com>
2023-08-02 12:29:00 +1200

    fixed telegram demo

--- 15/Delphi/IWTelegramDemo/Unit1.pas
+++ 15/Delphi/IWTelegramDemo/Unit1.pas
@@ -5,7 +5,7 @@ interface
 uses
   Classes, SysUtils, IWAppForm, IWApplication, IWColor, IWTypes, IWCompButton,
   IWCompLabel, Vcl.Controls, IWVCLBaseControl, IWBaseControl, IWBaseHTMLControl,
-  IWControl, IWCompMemo, IWHTMLControls, Vcl.Menus, IWTMSPopup, IWDiscord;
+  IWControl, IWCompMemo, IWHTMLControls, Vcl.Menus;
 
 type
   TIWForm1 = class(TIWAppForm)
@@ -32,13 +32,11 @@ uses
 const
   BOT_TOKEN = '1633190066:AAEfdo6vQzB7KjYjJYyMJU1hA8V3OUANhW4';
   CHAT_ID = '@IWTelegramComponent';
-  PIXPDV_Erros = '1071116067608461332/I_Zyg_9PI6YEfWMYRRHrxdlXXDBf_QJJz5q3_ZxrD3LX-p1-clxUFm4ugN4553Pjp2EU';
 
 procedure TIWForm1.IWButton2AsyncClick(Sender: TObject;
   EventParams: TStringList);
 begin
-  TIWDiscordHelper.SendMessageAsync(PIXPDV_Erros, IWMemo1.Lines.Text, 'Gilmar Mendes');
-  //TIWTelegramHelper.SendMessageAsync(BOT_TOKEN, CHAT_ID, IWMemo1.Lines.Text);
+  TIWTelegramHelper.SendMessageAsync(BOT_TOKEN, CHAT_ID, IWMemo1.Lines.Text);
   IWMemo1.Lines.Clear;
   WebApplication.ShowMessage('Your message has been sent!');
 end;
