a1cb15013808496a20e6e0b5d1f74394bc1460ea
Alexandre Machado <alexandre@atozed.com>
2025-03-03 22:21:50 +1300

    PostDataDemo ASPX project

---
+++ 15/Delphi/PostDataDemo/PostDataDemo_ASPX.dpr
@@ -0,0 +1,18 @@
+library PostDataDemo_ASPX;
+
+uses
+  IWRtlFix,
+  IWJclStackTrace,
+  IWJclDebug,
+  Forms,
+  IW.Export,
+  Unit2 in 'Unit2.pas' {IWForm2: TIWAppForm},
+  ServerController in 'ServerController.pas' {IWServerController: TIWServerControllerBase},
+  UserSessionUnit in 'UserSessionUnit.pas' {IWUserSession: TIWUserSessionBase},
+  IW.Content.XML in 'IW.Content.XML.pas';
+
+{$R *.res}
+
+begin
+
+end.
