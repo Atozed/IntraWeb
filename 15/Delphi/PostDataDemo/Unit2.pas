605f43369ceb131510e400d5054e66d87c2ba7a7
Alexandre Machado <alexandre@atozed.com>
2025-03-18 19:54:58 +1300

    Small UI change

--- 15/Delphi/PostDataDemo/Unit2.pas
+++ 15/Delphi/PostDataDemo/Unit2.pas
@@ -3,10 +3,15 @@ unit Unit2;
 interface
 
 uses
-  Classes, SysUtils, IWAppForm, IWApplication, IWColor, IWTypes;
+  Classes, SysUtils, IWAppForm, IWApplication, IWColor, IWTypes, IWCompLabel,
+  Vcl.Imaging.pngimage, Vcl.Controls, IWVCLBaseControl, IWBaseControl,
+  IWBaseHTMLControl, IWControl, IWCompExtCtrls;
 
 type
   TIWForm2 = class(TIWAppForm)
+    IWImage1: TIWImage;
+    IWLabel1: TIWLabel;
+    IWLabel2: TIWLabel;
   public
   end;
 
